f40524d94e67af59a4b42116b2c35c0f12693ad5
Jason Pero <jpero09@gmail.com>
2015-09-19 13:21:34 +0000

    style changes. card filtering

--- precompile/app/cards/controller.js
+++ precompile/app/cards/controller.js
@@ -8,6 +8,7 @@
   /* @ngInject */
   function cardsController($scope, Cards) {
     $scope.isLoading = true;
+    $scope.filter = {rarity: {}, range: {}, affinity: {}};
 
     $scope.init = function() {
       Cards.GetAll({},
@@ -35,6 +36,52 @@
       }
 
       return output;
+    };
+
+    $scope.explorerFilter = function(item) {
+      var f = $scope.filter;
+
+      if(!item) { return false; }
+
+      // jscs:disable requireCamelCaseOrUpperCaseIdentifiers
+      if(f.skill && !item.skill_id) { return false; }
+
+      if(f.support && !item.support_id) { return false; }
+
+      if(f.junkyard && !item.is_junkyard_exclusive) { return false; }
+      // jscs:enable requireCamelCaseOrUpperCaseIdentifiers
+
+      if(f.name) {
+        var searchable = (item.name + item.nickname).toLowerCase();
+        if(searchable.indexOf(f.name.toLowerCase()) < 0) {
+
+          return false;
+        }
+      }
+
+      // Alignment
+      if(f.affinity.l || f.affinity.d || f.affinity.n) {
+        var side = item.side.toLowerCase();
+
+        if(side === 'l' && !f.affinity.l) { return false; }
+
+        if(side === 'd' && !f.affinity.d) { return false; }
+
+        if(side === 'n' && !f.affinity.n) { return false; }
+      }
+
+      // Range
+      if(f.range.s || f.range.m || f.range.l) {
+        var range = item.range.toLowerCase();
+
+        if(range === 's' && !f.range.s) { return false; }
+
+        if(range === 'm' && !f.range.m) { return false; }
+
+        if(range === 'l' && !f.range.l) { return false; }
+      }
+
+      return true;
     };
 
     $scope.init();
